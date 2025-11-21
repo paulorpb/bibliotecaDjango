e2fca179a4999ea6ac3b97ebae3176818bbde083
Adriano Bispo <drianvini10@gmail.com>
2025-11-21 14:32:36 -0300

    Add viewsets for Autor, Livro, and Pedido models

--- core/views.py
+++ core/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Autor, Livro, Pedido
+from .serializers import AutorSerializer, LivroSerializer, PedidoSerializer
 
-# Create your views here.
+class AutorViewSet(viewsets.ModelViewSet):
+    queryset = Autor.objects.all()
+    serializer_class = AutorSerializer
+
+class LivroViewSet(viewsets.ModelViewSet):
+    queryset = Livro.objects.all()
+    serializer_class = LivroSerializer
+
+class PedidoViewSet(viewsets.ModelViewSet):
+    queryset = Pedido.objects.all()
+    serializer_class = PedidoSerializer
