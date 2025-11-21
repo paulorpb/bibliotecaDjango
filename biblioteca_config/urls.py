7cd2d68c2550dce5b36ba67d19cf4fbb8db2ba21
Adriano Bispo <drianvini10@gmail.com>
2025-11-21 14:33:02 -0300

    Add URL routing for API endpoints of Autor, Livro, and Pedido models

--- biblioteca_config/urls.py
+++ biblioteca_config/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from core.views import AutorViewSet, LivroViewSet, PedidoViewSet
+
+router = DefaultRouter()
+router.register(r'autores', AutorViewSet)
+router.register(r'livros', LivroViewSet)
+router.register(r'pedidos', PedidoViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
